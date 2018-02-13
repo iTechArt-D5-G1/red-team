425006d194bc56f43129a9dfc1477f7617cd3166
Uladzimir Masla <uladzimir.masla@itechart-group.com>
2018-02-13 20:13:32 +0300

    MainPageContent fix

--- frontend/src/Components/MainPageContent.js
+++ frontend/src/Components/MainPageContent.js
@@ -1,39 +1,17 @@
-import axios from 'axios';
 import React, { Component } from 'react';
 import FormToSubmit from '../containers/FormToSubmit';
 import GetSurveysList from '../containers/GetSurveysList';
 import { addSurvey } from '../actions/action-creators';
-import { store } from '../index';
-
-const ServerURL = 'http://http://localhost:8081/';
-
-class MainPageContent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            test: null,
-        };
-    }
-
-    callback(data) {
-        alert('Not implemented part: wait till server side is enable');
-        const requestData = axios.get(`${ServerURL}MethodName/${data}`);
-    }
 
 
-    render() {
-        return (
-            <div className='container main_page'>
-                <div className='row'>
-                    <FormToSubmit />
-                </div>
+const MainPageContent = () =>(
+    <div className='container main_page'>
+        <div className='row'>
+            <FormToSubmit />
+        </div>
 
-                <GetSurveysList />
-
-            </div>
-        );
-    }
-}
-
+    <GetSurveysList />
+</div>
+)
 
 export default MainPageContent;
