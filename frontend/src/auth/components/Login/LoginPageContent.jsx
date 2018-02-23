74e8c9dcc32ee992520a9480d27f7fb569c57075
Uladzimir Masla <uladzimir.masla@itechart-group.com>
2018-02-23 11:04:11 +0300

    Fix BEM convention in Login form

--- frontend/src/auth/components/Login/LoginPageContent.jsx
+++ frontend/src/auth/components/Login/LoginPageContent.jsx
@@ -4,10 +4,10 @@ import './forms.scss';
 
 const LoginPageContent = onSubmit => (
     <div className='container' onSubmit={onSubmit}>
-        <form className='login_form'>
-            <p><input className='login_form__login_imput' type='text' /></p>
-            <p><input className='login_form__password_input' type='text' /></p>
-            <button >Login</button>
+        <form className='form'>
+            <p><input className='form__login_imput' type='text' /></p>
+            <p><input className='form__password_input' type='text' /></p>
+            <button className='form__login_btn'>Login</button>
         </form>
     </div>
 );
