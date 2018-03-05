c9202d7b5c9533056573f9dd2c887943f7bdaf55
Uladzimir Masla <uladzimir.masla@itechart-group.com>
2018-03-05 17:41:52 +0300

    Fix BEM elements names

--- frontend/src/auth/components/Login/LoginPage.jsx
+++ frontend/src/auth/components/Login/LoginPage.jsx
@@ -5,9 +5,9 @@ import './forms.scss';
 const LoginPage = onSubmit => (
     <section className='container' onSubmit={onSubmit}>
         <form className='form'>
-            <p><input className='form__login_imput' type='text' /></p>
-            <p><input className='form__password_input' type='text' /></p>
-            <button className='form__login_btn'>Login</button>
+            <p><input className='form__login-imput' type='text' /></p>
+            <p><input className='form__password-input' type='text' /></p>
+            <button className='form__login-btn'>Login</button>
         </form>
     </section>
 );
