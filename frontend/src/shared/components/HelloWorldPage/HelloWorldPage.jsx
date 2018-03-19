15b9a0f648c9d12257620ea322aaf55148fbd7f1
Uladzimir Masla <uladzimir.masla@itechart-group.com>
2018-03-19 19:31:22 +0300

    Fix HelloWorld css

--- frontend/src/shared/components/HelloWorldPage/HelloWorldPage.jsx
+++ frontend/src/shared/components/HelloWorldPage/HelloWorldPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import './hello-world-page.scss';
+import helloWorldPageBackground from './hello-world-page-background.png';
 
 const HelloWorldPage = () => (
     <section className='hello-world-page'>
-        <p className='hello-world-page__content'>Hello World!</p>
+        <p className='hello-world-page__text'>Hello World!</p>
+        <img src={helloWorldPageBackground} alt='Error during load' className='hello-world-page__image' />
     </section>
 );
 
