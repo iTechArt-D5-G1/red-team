2cbd633948c0cf3c843ff4fa7e9817c2f0d979f8
Uladzimir Masla <uladzimir.masla@itechart-group.com>
2018-03-27 16:26:58 +0300

    Remove ID generator from surveys/actions.js

--- frontend/src/surveys/action.js
+++ frontend/src/surveys/action.js
@@ -6,13 +6,6 @@ export const SURVEYS_REQUEST_SUCCESS = 'SURVEYS_REQUEST_SUCCESS';
 export const SURVEYS_REQUEST_ERROR = 'SURVEYS_REQUEST_ERROR';
 export const ADD_SURVEY = 'ADD_SURVEY';
 
-let nextSurveyId = 0;
-
-function IdInc() {
-    nextSurveyId += 1;
-    return nextSurveyId;
-}
-
 export const surveyRequestSuccess = requestedData => ({
     type: SURVEYS_REQUEST_SUCCESS,
     surveys: requestedData,
@@ -39,7 +32,7 @@ export function surveysRequest() {
 export function addSurvey(text) {
     return async (dispatch) => {
         try {
-            const survey = await surveyService.addSurvey(new Survey(IdInc(), text));
+            const survey = await surveyService.addSurvey(new Survey(undefined, text));
             dispatch(surveyRequestSuccess(survey));
         } catch (err) {
             dispatch(surveyRequestError());
