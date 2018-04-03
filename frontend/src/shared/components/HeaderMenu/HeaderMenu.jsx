639100b00f3b47ad7c52af5f8c4fc370c98ad2fd
Uladzimir Masla <uladzimir.masla@itechart-group.com>
2018-04-03 17:27:27 +0300

    Replace string literals with routePath const in HeaderMenu component

--- frontend/src/shared/components/HeaderMenu/HeaderMenu.jsx
+++ frontend/src/shared/components/HeaderMenu/HeaderMenu.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { surveyRootPath, helloWorldPagePath } from '../../../shared/routePath';
 import './HeaderMenu.scss';
 
 const HeaderMenu = () => (
     <header>
         <nav>
             <ul className='menu'>
-                <li className='menu__btn'><Link to='/' className='menu__link'>Main Page</Link></li>
-                <li className='menu__btn'><Link to='/hello' className='menu__link'>Second Page</Link></li>
+                <li className='menu__btn'><Link to={surveyRootPath} className='menu__link'>Main Page</Link></li>
+                <li className='menu__btn'><Link to={helloWorldPagePath} className='menu__link'>Hello world Page</Link></li>
             </ul>
         </nav>
     </header>
