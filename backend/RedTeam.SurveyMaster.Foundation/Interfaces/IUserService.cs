2a69ab70cfd1c8f28a534cefc01d602206ea18bb
Vova Maslo <VladimirMaslo@tut.by>
2018-04-25 18:44:24 +0300

    Add IUserService and UserService

---
+++ backend/RedTeam.SurveyMaster.Foundation/Interfaces/IUserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedTeam.SurveyMaster.Repositories.Models;
+
+namespace RedTeam.SurveyMaster.Foundation.Interfaces
+{
+    public interface IUserService
+    {
+        User Add(User user);
+
+        void Update(User user);
+
+        void Delete(User user);
+
+        Task<User> GetByIdAsync(int id);
+
+        IQueryable<User> GetAllUsers();
+
+        Task SaveChangesAsync();
+
+    }
+}
