f437d3c44f59b3aa51cd655b969da9d0a1d242dd
Vova Maslo <VladimirMaslo@tut.by>
2018-04-25 18:40:14 +0300

    Add UserDbContext

---
+++ backend/RedTeam.SurveyMaster.Repositories/UserDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedTeam.Repositories.Interfaces;
+using RedTeam.SurveyMaster.Repositories.Models;
+
+namespace RedTeam.SurveyMaster.Repositories
+{
+    public class UserDbContext : DbContext, IDbContext
+    {
+        private const string ConnectionStringName = "UsersRepository";
+
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public UserDbContext()
+            : base(ConnectionStringName)
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+    }
+}
