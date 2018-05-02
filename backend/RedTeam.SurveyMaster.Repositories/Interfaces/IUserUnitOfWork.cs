5b8d7665b957660eeaccacc6c2ad618e33283975
Vova Maslo <VladimirMaslo@tut.by>
2018-05-02 21:00:12 +0300

    Remove unused namespaces

--- backend/RedTeam.SurveyMaster.Repositories/Interfaces/IUserUnitOfWork.cs
+++ backend/RedTeam.SurveyMaster.Repositories/Interfaces/IUserUnitOfWork.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RedTeam.Repositories.Interfaces;
+﻿using RedTeam.Repositories.Interfaces;
 
 namespace RedTeam.SurveyMaster.Repositories.Interfaces
 {
