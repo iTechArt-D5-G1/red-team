05913dd05a89508f25d015020b5f071b3f3407ad
Vova Maslo <VladimirMaslo@tut.by>
2018-05-04 21:08:36 +0300

    Add UserDto

---
+++ backend/RedTeam.SurveyMaster.Repositories/DataTransferObjectModels/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RedTeam.SurveyMaster.Repositories.DataTransferObjectModels
+{
+    public class UserDto
+    {
+        [Required(ErrorMessage = "You should set Email")]
+        [EmailAddress(ErrorMessage = "Email isn't valid")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "You should set Password")]
+        [MinLength(6, ErrorMessage = "Min length of password is 6 symbols")]
+        public string Password { get; set; }
+    }
+}
