f9cb9a8ab2e0ef848f83dc66ee139ff1ad507e82
Vova Maslo <VladimirMaslo@tut.by>
2018-05-08 18:00:30 +0300

    Add ApiError and ApiErrorResult

---
+++ backend/RedTeam.SurveyMaster.WebApi/Errors/ApiError.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Http.ModelBinding;
+
+namespace RedTeam.SurveyMaster.WebApi.Errors
+{
+    public class ApiError
+    {
+        public string Code;
+
+        public string Message;
+
+        public ICollection<KeyValuePair<string, ModelState>> Description;
+
+
+        public ApiError(string code, string message = "", ICollection<KeyValuePair<string, ModelState>> descriprion = null)
+        {
+            Code = code;
+            Message = message;
+            Description = descriprion;
+        }
+    }
+}
---
+++ backend/RedTeam.SurveyMaster.WebApi/Errors/ApiErrorResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace RedTeam.SurveyMaster.WebApi.Errors
+{
+    public class ApiErrorResult : IHttpActionResult
+    {
+        private readonly ApiError _error;
+
+        private readonly HttpStatusCode _statusCode;
+
+
+        public ApiErrorResult(HttpStatusCode statusCode, ApiError error )
+        {
+            _statusCode = statusCode;
+            _error = error;
+        }
+
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(_statusCode);
+            var serializedErrorObject = JsonConvert.SerializeObject(_error);
+            response.Content = new StringContent(serializedErrorObject, Encoding.UTF8, "application/json");
+            return Task.FromResult(response);
+        }
+    }
+}
