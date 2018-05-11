847ef543f54bcd4c0c5e16ffd1690128d794ef26
Vova Maslo <VladimirMaslo@tut.by>
2018-05-11 17:56:57 +0300

    Remove useless namespaces

--- backend/RedTeam.SurveyMaster.Repositories/Migrations/201805031334535_Initial.cs
+++ backend/RedTeam.SurveyMaster.Repositories/Migrations/201805031334535_Initial.cs
@@ -1,8 +1,7 @@
 namespace RedTeam.SurveyMaster.Repositories.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class Initial : DbMigration
     {
         public override void Up()
