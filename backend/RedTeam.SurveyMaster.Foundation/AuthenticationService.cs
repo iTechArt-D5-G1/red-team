7b42de1e3b10be1342ff352369885cc2e720efca
Vova Maslo <VladimirMaslo@tut.by>
2018-05-23 17:40:45 +0300

    Add AuthenticationService

---
+++ backend/RedTeam.SurveyMaster.Foundation/AuthenticationService.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using RedTeam.Common.Interfaсes;
+using RedTeam.SurveyMaster.Foundation.Interfaces;
+
+namespace RedTeam.SurveyMaster.Foundation
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly ITokenService _tokenService;
+
+        private readonly IUserManagerFactory _userManagerFactory;
+
+
+        public AuthenticationService(ITokenService tokenService, IUserManagerFactory userManagerFactory)
+        {
+            _tokenService = tokenService;
+            _userManagerFactory = userManagerFactory;
+        }
+
+
+        public string CreateToken(string userName, string userRoleName)
+        {
+            var token = _tokenService.CreateSecurityToken(userName, userRoleName);
+            return _tokenService.SerializeToken(token);
+        }
+
+        public async Task<bool> IsUserExistsAsync(string userName, string password)
+        {
+            var userManager = _userManagerFactory.GetUserManager();
+            return await userManager.FindAsync(userName, password) != null;
+        }
+
+        public async Task<string> GetUserRoleNameAsync(string userName)
+        {
+            var userManager = _userManagerFactory.GetUserManager();
+            var user = await userManager.FindByNameAsync(userName);
+            var userRoleName = userManager.GetRoles(user.Id).FirstOrDefault();
+            return userRoleName;
+        }
+    }
+}
---
+++ backend/RedTeam.SurveyMaster.Foundation/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace RedTeam.SurveyMaster.Foundation.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        string CreateToken(string userName, string userRoleName);
+
+        Task<bool> IsUserExistsAsync(string userName, string password);
+
+        Task<string> GetUserRoleNameAsync(string userName);
+
+    }
+}
