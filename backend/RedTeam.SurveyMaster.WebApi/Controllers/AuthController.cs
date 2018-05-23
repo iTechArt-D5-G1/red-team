478fed99e541d208d5c541093f2e76128affb098
Vova Maslo <VladimirMaslo@tut.by>
2018-05-23 17:45:03 +0300

    Refactor AuthController

--- backend/RedTeam.SurveyMaster.WebApi/Controllers/AuthController.cs
+++ backend/RedTeam.SurveyMaster.WebApi/Controllers/AuthController.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
-using System.Web.Http.ModelBinding;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using RedTeam.Common.Interfaсes;
+using RedTeam.Common.ExtentionMethods;
 using RedTeam.SurveyMaster.Foundation.Interfaces;
-using RedTeam.SurveyMaster.Repositories;
-using RedTeam.SurveyMaster.Repositories.Models;
 using RedTeam.SurveyMaster.WebApi.DataTransferObjectModels;
 using RedTeam.SurveyMaster.WebApi.Errors;
 
@@ -20,32 +12,30 @@ namespace RedTeam.SurveyMaster.WebApi.Controllers
     public class AuthController : ApiController
     {
 
-        private readonly ITokenService _tokenService;
+        private readonly IAuthenticationService _authenticationService;
 
 
-        public AuthController(ITokenService tokenService)
+        public AuthController(IAuthenticationService authenticationService)
         {
-            _tokenService = tokenService;
+            _authenticationService = authenticationService;
         }
 
 
         [HttpPost]
         public async Task<IHttpActionResult> Authenticate([FromBody] UserDto userInfo)
         {
-            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<UserManager>();
 
             if (!ModelState.IsValid)
             {
                 ApiError error = new ApiError("invalid_credentials", "Invalid user credentials",
-                    FetchErrorsFromModelState(ModelState));
+                    ModelState.FetchErrorsFromModelState());
                 return new ApiErrorResult(HttpStatusCode.BadRequest, error);
             }
 
-            if (await userManager.FindByEmailAsync(userInfo.Username) != null)
+            if (await _authenticationService.IsUserExistsAsync(userInfo.Username, userInfo.Password))
             {
-                var userFromUserManager = await userManager.FindByEmailAsync(userInfo.Username);
-                var userRole = userManager.GetRoles(userFromUserManager.Id).FirstOrDefault();
-                string token = CreateToken(userInfo.Username, userRole);
+                var userRoleName = await _authenticationService.GetUserRoleNameAsync(userInfo.Username);
+                string token = _authenticationService.CreateToken(userInfo.Username, userRoleName);
                 return Ok(token);
             }
             else
@@ -53,29 +43,6 @@ namespace RedTeam.SurveyMaster.WebApi.Controllers
                 ApiError error = new ApiError("user_not_found", "User with setted params not found");
                 return new ApiErrorResult(HttpStatusCode.NotFound, error);
             }
-        }
-
-
-        private string CreateToken(string userName, string userRoleName)
-        {
-            var token = _tokenService.CreateSecurityToken(userName, userRoleName);
-            return _tokenService.SerializeToken(token);
-        }
-
-        private static List<string> FetchErrorsFromModelState(ModelStateDictionary modelStateDictionary)
-        {
-            List<string> errorDescriptionList =
-                new List<string>();
-
-            foreach (var modelState in modelStateDictionary)
-            {
-                foreach (var error in modelState.Value.Errors)
-                {
-                    errorDescriptionList.Add(error.ErrorMessage);
-                }
-            }
-
-            return errorDescriptionList;
         }
     }
 }
