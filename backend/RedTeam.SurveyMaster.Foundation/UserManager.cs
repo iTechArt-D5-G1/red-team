db98d32df6c25621928fd71692f4343d61c3613c
Vova Maslo <VladimirMaslo@tut.by>
2018-05-24 21:19:51 +0300

    Relocate UserManager from Repositories to Foundation layer

---
+++ backend/RedTeam.SurveyMaster.Foundation/UserManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity;
+using RedTeam.SurveyMaster.Repositories.Models;
+
+namespace RedTeam.SurveyMaster.Foundation
+{
+    public class UserManager : UserManager<User>
+    {
+        public UserManager(IUserStore<User> store)
+            : base(store)
+        {
+        }
+    }
+}
