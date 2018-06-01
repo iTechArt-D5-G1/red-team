97defed4c11b24e2a6ffa67b406e6fba7ced846b
Sviataslau Ryukou <sviataslau.ryukou@itechart-group.com>
2018-06-01 16:48:48 +0300

    Rearrange AuthService methods to async/await

--- frontend/src/authentication/services/authService.js
+++ frontend/src/authentication/services/authService.js
@@ -6,40 +6,32 @@ export class AuthService {
         this.http = http;
     }
 
-    SignOutUser() {
-        return (dispatch) => {
-            dispatch(unSignInUser());
-            localStorage.removeItem('token');
-        };
+    static SignOutUser(dispatch) {
+        dispatch(unSignInUser());
+        localStorage.removeItem('token');
     }
 
-    SignInUser(email, password) {
-        return (dispatch) => {
-            this.http.post('/register', {
-                email, password,
-            })
-                .then((response) => {
-                    localStorage.setItem('token', response.data.token);
-                    dispatch(signInUser());
-                })
-                .catch((error) => {
-                    errorHandler(error.response, signInError());
-                });
-        };
+    async SignInUser(email, password, dispatch) {
+        try {
+            const response = await this.http.post('/register', { email, password });
+            localStorage.setItem('token', response.data.token);
+            dispatch(signInUser());
+        } catch (error) {
+            errorHandler(dispatch, error.response, signInError());
+        }
     }
 
-    RegisterUser(dispatch, {
+    async RegisterUser(dispatch, {
         email, firstName, lastName, password,
     }) {
-        this.http.post('/register', {
-            email, firstName, lastName, password,
-        })
-            .then((response) => {
-                localStorage.setItem('token', response.data.token);
-                dispatch(signInUser());
-            })
-            .catch((error) => {
-                errorHandler(dispatch, error.response, signInError());
+        try {
+            const response = await this.http.post('/register', {
+                email, firstName, lastName, password,
             });
+            localStorage.setItem('token', response.data.token);
+            dispatch(signInUser());
+        } catch (error) {
+            errorHandler(dispatch, error.response, signInError());
+        }
     }
 }
