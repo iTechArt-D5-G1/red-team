c309c1c50be2c4d3af5e3c260636ea2fb3d4e23b
Vova Maslo <VladimirMaslo@tut.by>
2018-06-01 17:23:03 +0300

    Add ClaimsPrincipalExtention class

---
+++ backend/RedTeam.Common/ExtentionMethods/ClaimsPrincipalExtention.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace RedTeam.Common.ExtentionMethods
+{
+    public static class ClaimsPrincipalExtention
+    {
+        public static Claim FetchAuthenticationClaim(this ClaimsPrincipal claimsPrincipal)
+        {
+            return FetchClaimByClaimType(claimsPrincipal, ClaimTypes.Authentication);
+        }
+
+        public static Claim FetchNameClaim(this ClaimsPrincipal claimsPrincipal)
+        {
+            return FetchClaimByClaimType(claimsPrincipal, ClaimTypes.Name);
+        }
+
+        public static Claim FetchRoleClaim(this ClaimsPrincipal claimsPrincipal)
+        {
+            return FetchClaimByClaimType(claimsPrincipal, ClaimTypes.Role);
+        }
+
+
+        private static Claim FetchClaimByClaimType(ClaimsPrincipal claimsPrincipal, string claimType)
+        {
+            var claim = claimsPrincipal.Claims.FirstOrDefault(x => x.Type == claimType);
+            return claim;
+        }
+    }
+}
